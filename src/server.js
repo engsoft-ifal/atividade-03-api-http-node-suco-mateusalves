c18b39cdb56e1cc140fdc84c8f868b0b95fefcd2
Mateus Alves <153127373+suco-mateusalves@users.noreply.github.com>
2026-02-26 13:47:11 -0300

    implementa rota GET /health

--- src/server.js
+++ src/server.js
@@ -5,3 +5,8 @@ const server = http.createServer(() => {})
 server.listen(3000, () => {
   console.log('Servidor rodando na porta 3000')
 })
+
+if (req.method === 'GET' && req.url === '/health') {
+  res.writeHead(200, { 'Content-Type': 'application/json' })
+  return res.end(JSON.stringify({ status: 'ativo' }))
+}
